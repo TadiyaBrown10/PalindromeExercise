b3cc15f562442773dd54e93fecca073b4f080520
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-05-08 14:00:54 -0500

    palindrome exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+
+		public bool IsAPalindrome(string word)
+		{
+			var Reverse = "";
+			for(int i = word.Length - 1; i >= 0; i--)
+			{
+				Reverse += word[i];
+			}
+
+			if (Reverse.ToLower() == word.ToLower())
+			{
+				return true;
+			}
+
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("sure", false)]
+        [InlineData("what", false)]
+        [InlineData("hello", false)]
 
+        public void myTester(string word, bool expected)
+        {
+            var tester = new WordSmith();
+
+            bool actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
+
     }
 }
